4ef4d360651ca48f910072ba1053af210a1e9b41
marcelaOrellana <marcelapaz.1985@gmail.com>
2017-06-28 14:52:12 -0400

    agrega a listos

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,10 +7,24 @@ $(document).ready(function(){
       if(name == ""){
 		    alert("Debes escribir tu nombre y el ensaje");
 	    }else{
-	    	alert(name);
-	    	$("#lista").append("<div><input type='checkbox' id='test" + cont +"'/><label for='test" + cont + "'>" + name + "</label><button class='remove'>Remove</button>");
+	    	//alert(name);
+	    	$("#lista").append("<div><input type='checkbox' id='test" + cont +"'/><label for='test" + cont + "'>" + "<p>" + name +"</p>" + "</label><button class='remove'>Remove</button>");
 	    	cont++;
 	    }
     }
   });
+
+    $('#lista').on("change", ":checkbox", function () {
+        if (this.checked) {
+        	var ide = this.id;
+            console.log(this.id + ' is checked  ' +$(this).siblings().find("p").text() );
+            //var str = $( "p:first" ).text();
+            //$( "p:last" ).html( str );
+            $("#completed").append($(this).parent().clone());
+
+        } else {
+            console.log(this.id + ' is unchecked');
+        }
+    });
+
 });
