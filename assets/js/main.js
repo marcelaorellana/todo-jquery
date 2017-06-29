58fd35282070eef5da1a34d6764853f561e5440b
marcelaorellana <marcelapaz.1985@gmail.com>
2017-06-28 23:02:22 -0400

    algunos estilos

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,13 +9,14 @@ $(document).ready(function(){
        e.preventDefault();
       var name = $("#tarea-nueva").val();
       if(name == ""){
-		    alert("Debes escribir tu nombre y el ensaje");
+		    alert("Debes escribir la tarea");
 	    }else{
 	    	//alert(name);
-	    	$("#lista").append("<div><input type='checkbox' id='test" + cont +"'/><label for='test" + cont + "'>" + "<p>" + name +"</p>" + "</label><button class='remove'>Remove</button>");
+	    	$("#lista").append("<div><input type='checkbox' id='test" + cont +"'/><label for='test" + cont + "'>" + "<p>" + name +"</p>" + "</label><button class='remove'><i class='fa fa-trash' aria-hidden='true'></i></button>");
 	    	cont++;
         console.log(cont);
         localStorage.setItem("on_load_counter", cont);
+        name = "";
 	    }
     }
   });
@@ -37,5 +38,13 @@ $(document).ready(function(){
     $(document).on("click", ".remove",function(){
       $(this).parent().empty();
     });
+  $(".mat-input").focus(function(){
+  $(this).parent().addClass("is-active is-completed");
+});
 
+$(".mat-input").focusout(function(){
+  if($(this).val() === "")
+    $(this).parent().removeClass("is-completed");
+  $(this).parent().removeClass("is-active");
+})
 });
