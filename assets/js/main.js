27aad51eda63e312d52098c8b33fd3cf91e63a76
marcelaOrellana <marcelapaz.1985@gmail.com>
2017-06-29 10:32:39 -0400

    localstorage a las tareas, solo muestra en lista de tareas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
-  var cont = localStorage.getItem('on_load_counter');
+  //var cont = localStorage.getItem('contador');
+  var cont = localStorage.length;
   if (cont === null) {
     cont = 1;
-}
+  } 
   $(document).keypress(function(e){
    
     if(e.which == 13) {
@@ -11,52 +12,63 @@ $(document).ready(function(){
       if(name == ""){
 		    alert("Debes escribir la tarea");
 	    }else{
-	    	//alert(name);
 	    	$("#lista").append("<div class='task-area'><input type='checkbox' id='test" + cont +"'/><label for='test" + cont + "'>" + "<p>" + name +"</p>" + "</label><button class='remove'><i class='fa fa-trash' aria-hidden='true'></i></button></div>");
 	    	cont++;
         console.log(cont);
-        localStorage.setItem("on_load_counter", cont);
+        localStorage.setItem(cont, name);
         $("#tarea-nueva").val("");
-        
 	    }
     }
   });
 
-    $('#lista').on("change", ":checkbox", function () {
-        if (this.checked) {
-        	var ide = this.id;
-            console.log(this.id + ' is checked  ');
-            $(this).parent().appendTo("#completed");
-            //$("#completed").append($(this).parent().clone());
-            //$(this).parent().empty();
+  $('#lista').on("change", ":checkbox", function () {
+      if (this.checked) {
+      	var ide = this.id;
+          console.log(this.id + ' is checked  ');
+          $(this).parent().appendTo("#completed");
+          //$("#completed").append($(this).parent().clone());
+          //$(this).parent().empty();
 
-        } else {
-            console.log(this.id + ' is unchecked');
-        }
-    });
-    $('#completed').on("change", ":checkbox", function () {
-        if (this.checked) {
-          var ide = this.id;
-            console.log(this.id + ' is checked  ');
-            //$("#completed").append($(this).parent().clone());
-            //$(this).parent().empty();
+      } else {
+          console.log(this.id + ' is unchecked');
+      }
+  });
+  $('#completed').on("change", ":checkbox", function () {
+      if (this.checked) {
+        var ide = this.id;
+          console.log(this.id + ' is checked  ');
+          //$("#completed").append($(this).parent().clone());
+          //$(this).parent().empty();
 
-        } else {
-            console.log(this.id + ' is unchecked');
-            $(this).parent().appendTo("#lista");
-        }
-    });
+      } else {
+          console.log(this.id + ' is unchecked');
+          $(this).parent().appendTo("#lista");
+      }
+  });
 
-    $(document).on("click", ".remove",function(){
-      $(this).parent().empty();
-    });
+
+  $(document).on("click", ".remove",function(){
+    $(this).parent().empty();
+    localStorage.removeItem(key);
+  });
+
+    //efectos del input
   $(".mat-input").focus(function(){
-  $(this).parent().addClass("is-active is-completed");
-});
+    $(this).parent().addClass("is-active is-completed");
+  });
 
-$(".mat-input").focusout(function(){
+  $(".mat-input").focusout(function(){
   if($(this).val() === "")
     $(this).parent().removeClass("is-completed");
-  $(this).parent().removeClass("is-active");
-})
+    $(this).parent().removeClass("is-active");
+  });
+
+
+
+
+  for(var i = 0; i < localStorage.length; i++){
+    var kei = localStorage.key(i);
+    var valuee = localStorage.getItem(kei);
+    $("#lista").append("<div class='task-area'><input type='checkbox' id='test" + kei +"'/><label for='test" + kei + "'>" + "<p>" + valuee +"</p>" + "</label><button class='remove'><i class='fa fa-trash' aria-hidden='true'></i></button></div>"); 
+  }
 });
